bc252dbfa936ea4757eb4caadbf765e7070c6947
analogandi <andi@andi-tower.andi-tower>
2025-11-07 00:52:21 +0100

    cell instantiation test

--- src/top.vhdl
+++ src/top.vhdl
@@ -1,9 +1,8 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity tt_um_example is
-    generic (MAX_COUNT : natural := 10_000_000);
     port (
         ui_in   : in  std_logic_vector(7 downto 0);
         uo_out  : out std_logic_vector(7 downto 0);
@@ -16,7 +15,47 @@ entity tt_um_example is
     );
 end tt_um_example;
 
-architecture Behavioral of tt_um_example is
+architecture RTL of tt_um_example is
+
+    -- length of inverter chain (you can rename or adjust)
+    constant g_chainDepth : natural := 100;
+
+    -- inverter chain signals
+    signal chain : std_logic_vector(g_chainDepth downto 0);
+
+    -- synthesis attribute: prevent optimization
+    attribute keep : string;
+    attribute keep of chain : signal is "yes";
+
 begin
-	uo_out <= ui_in and uio_in;
-end Behavioral;
+
+    ---------------------------------------------------------------------
+    -- Assign chain input from ui_in(0)
+    ---------------------------------------------------------------------
+    chain(0) <= ui_in(0);
+
+    ---------------------------------------------------------------------
+    -- Generate the SKY130 inverter chain
+    ---------------------------------------------------------------------
+    GEN_INV : for i in 0 to g_chainDepth-1 generate
+        INV_HARD : entity sky130_fd_sc_hd__inv_1
+            port map (
+                A => chain(i),
+                Y => chain(i+1)
+            );
+    end generate GEN_INV;
+
+    ---------------------------------------------------------------------
+    -- Chain output drives uo_out(0)
+    ---------------------------------------------------------------------
+    uo_out(0) <= chain(g_chainDepth);
+
+    ---------------------------------------------------------------------
+    -- All unused outputs forced low / safe
+    ---------------------------------------------------------------------
+    uo_out(7 downto 1) <= (others => '0');
+    uio_out            <= (others => '0');
+    uio_oe             <= (others => '0');
+
+end RTL;
+
