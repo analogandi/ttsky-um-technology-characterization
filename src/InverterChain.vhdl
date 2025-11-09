1c3668788c6228e8d46638a22d4faa8c3008b62b
analogandi <andi@andi-tower.andi-tower>
2025-11-10 00:22:07 +0100

    Change generics to constants

--- src/InverterChain.vhdl
+++ src/InverterChain.vhdl
@@ -3,11 +3,6 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity InverterChain is
-    generic (
-        g_sim      : boolean := false;
-        chain_len  : natural := 1024;
-        sim_delay  : time := 5 ns
-    );
     port(
         chain_in   : in  std_logic;
         chain_mid  : out std_logic;
@@ -17,6 +12,10 @@ entity InverterChain is
 end InverterChain;
 
 architecture RTL of InverterChain is
+
+    constant g_sim     : boolean := false;
+    constant chain_len : natural := 1024;
+    constant sim_delay : time    := 5 ns;
 
     signal chain : std_logic_vector(chain_len downto 0);
     attribute keep : string;
