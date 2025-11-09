24387b75ac8ebab1abfd164a5546ef030c479d32
analogandi <andi@andi-tower.andi-tower>
2025-11-09 19:02:45 +0100

    Reduce depth to reduce density

--- src/top.vhdl
+++ src/top.vhdl
@@ -32,7 +32,7 @@ architecture RTL of tt_um_example is
     ----------------------------------------------------------------------
     -- Inverter chain length
     ----------------------------------------------------------------------
-    constant g_chainDepth : natural := 4096;
+    constant g_chainDepth : natural := 1024;
 
     ----------------------------------------------------------------------
     -- Chain signals
